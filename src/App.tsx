bf800ce1530e933b21b21a939232ccce361e2285
j0101 <cavnav@gmail.com>
2020-06-14 22:14:57 +0300

    antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormSendRequest } from './components';
+import 'antd/dist/antd.css';
 import './App.css';
 
 
--- src/components/FormSendRequest/FormSendRequest.tsx
+++ src/components/FormSendRequest/FormSendRequest.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Form, Input } from 'antd';
 
 export function FormSendRequest({
 
 }) {
 
-  return <div>
-    FormSendRequest
-  </div>;
+  return <Form>
+    <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+      <Input />
+    </Form.Item>
+  </Form>;
 }   
